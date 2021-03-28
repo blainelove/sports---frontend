e6a71eeaa1f445bc05b01e70ed80c4a663711c6a
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-03-28 14:59:38 -0500

    url pic

--- src/components/VideoCards.js
+++ src/components/VideoCards.js
@@ -1,14 +1,17 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 
 
 const VideoCards = ({video}) => {
+    const [videos, setVideos] = useState(false)
    
+    
    
     return (
         <div>
             <h3>{video.title}</h3>
-            
+            <h2>{video.url}</h2>
+            <img src={video.pic}/>
         </div>
     )
 }
